e32bf66857e271bc6f739a37903bc5f2e655a39a
Unknown <Calvin.lapp@dcmail.ca>
2021-09-23 13:53:33 -0400

    server gucci

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,34 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
-  // run mgirations before anything else
+  // run migrations before anything else
   await orm.getMigrator().up();
-  // this does nothing with the DB atm
-  // const post = orm.em.create(Post, {title: 'My first post :)'})
-  // await orm.em.persistAndFlush(post);
 
-  // get all posts
-  const posts = await orm.em.find(Post, {});
-  console.log(posts)
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
-main();
+main().catch((err) => {
+  console.log(err);
+});
